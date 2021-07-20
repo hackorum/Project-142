cff24f111d5310b7ed6d11348583337a21d69ca9
Neil Sabde <neilsabde@gmail.com>
2021-07-20 09:13:40 +0530

    Remove something.csv

--- demographic.py
+++ demographic.py
@@ -2,5 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("english_articles.csv")
 df = df.sort_values("total_events", ascending=False)
-df.to_csv("something.csv")
 output = df.head(20).values.tolist()
